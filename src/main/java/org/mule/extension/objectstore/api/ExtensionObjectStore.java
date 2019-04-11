1427b740271da9a6dd120899f7e42518c83b7906
SebaElizalde <sebi.elizalde@gmail.com>
2019-04-11 12:25:04 -0300

    checking for null value now

--- src/main/java/org/mule/extension/objectstore/api/ExtensionObjectStore.java
+++ src/main/java/org/mule/extension/objectstore/api/ExtensionObjectStore.java
@@ -156,10 +156,12 @@ public abstract class ExtensionObjectStore implements ObjectStore<Serializable>,
     storeManagerProvider = getObjectStoreManagerProvider();
     objectStoreManager = getObjectStoreManager();
 
-    if (maxEntries < 0) {
-      LOGGER
-          .warn(format("The maxEntries parameter should not be negative (given value was: %d), otherwise there is no guarantee that the expiration policy will work correctly. For unlimited entries, just omit passing this parameter",
-                       maxEntries));
+    if (maxEntries != null) {
+      if (maxEntries < 0) {
+        LOGGER
+                .warn(format("The maxEntries parameter should not be negative (given value was: %d), otherwise there is no guarantee that the expiration policy will work correctly. For unlimited entries, just omit passing this parameter",
+                        maxEntries));
+      }
     }
 
     final ObjectStoreSettings.Builder settings = ObjectStoreSettings.builder()
