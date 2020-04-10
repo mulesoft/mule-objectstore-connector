b2533595f48e875a6b93abe3563536dc9f3d55d9
Ezequiel Werner <eze210@gmail.com>
2020-04-10 16:19:02 -0300

    Revert "MULE-18232: ObjectStore store operation shouldn't throw ObjectAlreadyExistsException if failIfPresent is set to false (#77)" (#78)
    This reverts commit 430a26dfaa34fc95bab2349acfc8a4784481efe8.

--- src/main/java/org/mule/extension/objectstore/internal/ObjectStoreOperations.java
+++ src/main/java/org/mule/extension/objectstore/internal/ObjectStoreOperations.java
@@ -113,14 +113,8 @@ public class ObjectStoreOperations {
                                                                                          createStaticMessage("ObjectStore already contains an object for key '"
                                                                                              + key + "'")));
         } else {
-          try {
-            os.remove(key);
-            os.store(key, value);
-          } catch (ObjectAlreadyExistsException | ObjectDoesNotExistException ex) {
-            // If we have a deficient lock, these operations aren't executed atomically, so we could see:
-            //   - An ObjectDoesNotExistException if some thread removed the key before the remove invocation.
-            //   - An ObjectAlreadyExistsException if some thread added the key between remove and store invocations.
-          }
+          os.remove(key);
+          os.store(key, value);
         }
       }
       return null;
