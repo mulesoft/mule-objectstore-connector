10282ea7ffea3191bc40bd9a1d3300a479f3fc37
Ezequiel Werner <eze210@gmail.com>
2020-11-02 17:47:01 -0300

    unused import

--- src/test/java/org/mule/extension/objectstore/internal/ExtensionObjectStoreTestCase.java
+++ src/test/java/org/mule/extension/objectstore/internal/ExtensionObjectStoreTestCase.java
@@ -13,7 +13,6 @@ import static org.hamcrest.core.Is.is;
 import static org.junit.rules.ExpectedException.none;
 import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
-import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 import static org.mockito.internal.verification.VerificationModeFactory.times;
