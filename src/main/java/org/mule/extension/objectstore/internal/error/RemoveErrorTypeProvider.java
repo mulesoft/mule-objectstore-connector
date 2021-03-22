0e9169463dc4ab01fef90113ccb33081d323b538
Nicolás Zdanovicz <nzdanovicz@mulesoft.com>
2021-03-22 15:21:18 -0300

    Cherrypicks 20210322 (#100)
    * Update dependency org.apache.maven.plugins:maven-resources-plugin to v3.2.0 (#92)
    
    Co-authored-by: Engineering Services Bot <engineering-services-bot@mulesoft.com>
    
    (cherry picked from commit 0a51af575346c5abe7040fefec1547fce777d02b)
    
    * Update munit.version to v2.3.2
    
    (cherry picked from commit 388af9e2f876387a5185e0b7488612c2d2ce80ca)
    
    * COBJCON-2: renovate settings for ignoring monthly builds
    
    (cherry picked from commit 8f02969b120c8c42321cdc98ceb7d8c009e5e366)
    
    * Update dependency com.mulesoft.munit:munit-extensions-maven-plugin to v1.1.0 (#93)
    
    * Update dependency com.mulesoft.munit:munit-extensions-maven-plugin to v1.1.0
    
    * renovate/munit.extensions.maven.plugin.version
    
    Co-authored-by: Engineering Services Bot <engineering-services-bot@mulesoft.com>
    Co-authored-by: Alejandro Curci <76498083+acurci-at-mulesoft@users.noreply.github.com>
    Co-authored-by: Alejandro Curci <acurci@mulesoft.com>
    (cherry picked from commit 080e02ea4351f29ecb2fa552827e105cc3b92466)
    
    * DEL-2788: Update mavenTestGoal to 'install' (#97)
    
    Co-authored-by: Alejandro Iannucci <4360246+aiannucci@users.noreply.github.com>
    
    (cherry picked from commit 95240f95e10eef15ded451a53b69d86543147648)
    
    * Update dependency com.mulesoft.munit:munit-extensions-maven-plugin to v1.1.1 (#96)
    
    Co-authored-by: Engineering Services Bot <engineering-services-bot@mulesoft.com>
    (cherry picked from commit 1487cd279b143a83385438e9b236cbced9584278)
    
    * DEL-2793: Fix & Update formatter-maven-plugin (#95)
    
    (cherry picked from commit 6d159b0f469232bed3c5a99f6ec2c362312c2e0a)
    
    * Fix/cobjcon 5 (#99)
    
    * COBJCON-5: Added ANY to the ErrorTypeProviders
    
    (cherry picked from commit b77266d37b8d2e6e211ad0d9419e26a6e93f06ea)
    
    Co-authored-by: renovate-pro-kbuild[bot] <56608192+renovate-pro-kbuild[bot]@users.noreply.github.com>
    Co-authored-by: Engineering Services Bot <engineering-services-bot@mulesoft.com>
    Co-authored-by: Alejandro Curci <acurci@mulesoft.com>
    Co-authored-by: Linda Vanessa Zapata Henao <28602809+lvzh90@users.noreply.github.com>
    Co-authored-by: Alejandro Iannucci <4360246+aiannucci@users.noreply.github.com>

--- src/main/java/org/mule/extension/objectstore/internal/error/RemoveErrorTypeProvider.java
+++ src/main/java/org/mule/extension/objectstore/internal/error/RemoveErrorTypeProvider.java
@@ -9,6 +9,8 @@ package org.mule.extension.objectstore.internal.error;
 import static org.mule.extension.objectstore.internal.error.ObjectStoreErrors.INVALID_KEY;
 import static org.mule.extension.objectstore.internal.error.ObjectStoreErrors.KEY_NOT_FOUND;
 import static org.mule.extension.objectstore.internal.error.ObjectStoreErrors.STORE_NOT_AVAILABLE;
+import static org.mule.runtime.extension.api.error.MuleErrors.ANY;
+
 import org.mule.runtime.extension.api.annotation.error.ErrorTypeProvider;
 import org.mule.runtime.extension.api.error.ErrorTypeDefinition;
 
@@ -32,6 +34,7 @@ public class RemoveErrorTypeProvider implements ErrorTypeProvider {
     errors.add(INVALID_KEY);
     errors.add(KEY_NOT_FOUND);
     errors.add(STORE_NOT_AVAILABLE);
+    errors.add(ANY);
 
     return errors;
   }
--- src/test/java/org/mule/extension/objectstore/internal/ObjectStoreOperationsTestCase.java
+++ src/test/java/org/mule/extension/objectstore/internal/ObjectStoreOperationsTestCase.java
@@ -14,18 +14,24 @@ import org.mockito.InjectMocks;
 import org.mockito.Matchers;
 import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
+import org.mule.runtime.api.i18n.I18nMessage;
 import org.mule.runtime.api.lock.LockFactory;
+import org.mule.runtime.api.metadata.TypedValue;
 import org.mule.runtime.api.store.ObjectDoesNotExistException;
 import org.mule.runtime.api.store.ObjectStore;
 import org.mule.runtime.api.store.ObjectStoreException;
 import org.mule.runtime.api.store.ObjectStoreManager;
 import org.mule.runtime.extension.api.exception.ModuleException;
 
+import java.io.Serializable;
 import java.util.concurrent.locks.ReentrantLock;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.junit.rules.ExpectedException.none;
+import static org.mockito.Matchers.any;
 import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.isA;
+import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -57,6 +63,25 @@ public class ObjectStoreOperationsTestCase {
     expectedException.expectMessage(containsString("ObjectStore 'objectStoreStringRepresentation'"));
 
     objectStoreOperations.retrieve("key", null, null);
+  }
+
+  @Test
+  public void rateLimitExceededRetrieve() throws ObjectStoreException {
+
+
+    when(lockFactory.createLock(anyString())).thenReturn(new ReentrantLock());
+
+    ObjectStore objectStore = mock(ObjectStore.class);
+    when(objectStore.toString()).thenReturn("objectStoreStringRepresentation");
+    when(runtimeObjectStoreManager.getDefaultPartition()).thenReturn(objectStore);
+    Exception e =
+        new Exception("Unable to check existence of object with key test in store APP_osdemo__defaultPersistentObjectStore, status code was 429, response was null");
+    when(objectStore.retrieve(Matchers.any())).thenThrow(new ObjectStoreException(e));
+
+    expectedException.expect(ModuleException.class);
+    expectedException.expectMessage(containsString("Rate Limit"));
+    objectStoreOperations.retrieve("key", null, null);
+
   }
 
 }
